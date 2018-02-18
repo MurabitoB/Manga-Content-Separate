770d6620266e7ccd73c43ebd6630045218c6e24f
MurabitoB <enemarykawai@gmail.com>
2018-02-18 17:30:17 +0800

    some update
    some update

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -2,11 +2,12 @@
 #include<vector>
 #include<iostream>
 #include<math.h>
+#include"image_information.h"
+#include<utility>
 using namespace std;
 using namespace cv;
-using namespace std;
-using namespace cv;
-
+vector <pair<Point, Point>>horL;
+vector <pair<Point, Point>>verL;
 class FindLines
 {
 private:
@@ -54,46 +55,66 @@ public:
 		return lines;
 	}
 
-	void drawDetectedLines(Mat &image, Scalar color = Scalar(0, 0, 255), int thickness = 4)
+	void drawDetectedLines(Mat &image, Scalar color = Scalar(0, 0, 255), int thickness = 2)
 	{
 		vector<Vec4i>::const_iterator it_lines = lines.begin();
 		while (it_lines != lines.end())
 		{
 			Point pt1((*it_lines)[0], (*it_lines)[1]);
 			Point pt2((*it_lines)[2], (*it_lines)[3]);
-			Point ptLeft(0, pt1.y);
-			Point ptRight(image.cols, pt1.y);
-			Point ptTop(pt1.x, 0);
-			Point ptBottom(pt1.x, image.rows);
-			/*if (abs(pt1.y - pt2.y) < 10.0)
+			if (abs(pt1.x - pt2.x) < 5 )
 			{
-				line(image, ptLeft, ptRight, color, thickness);
+				verL.push_back(make_pair(pt1, pt2));
+				line(image, pt1, pt2, color, thickness);
 			}
-			if (abs(pt1.x - pt2.x) < 30.0)
+			else if (abs(pt1.y - pt2.y) < 5)
 			{
-				line(image, ptTop, ptBottom, color, thickness);
-			}*/
-			line(image, pt1, pt2, color, thickness);
+				horL.push_back(make_pair(pt1, pt2));
+				line(image, pt1, pt2, color, thickness);
+			}
+
 			++it_lines;
 		}
 	}
 };
 
+
+void DetRoi()
+{
+	//統計出頂部
+}
+void getInformation(Mat src)
+{
+	src_height = src.rows;
+	src_width = src.cols;
+}
+
+void testLine(Mat img)
+{
+
+}
 int main()
 {
-	Mat img_src, img_gray, img_canny;
+	Mat img_src, img_gray, img_canny,img_bin;
 	FindLines linefinder;
-	img_src = imread("img020.jpg");
-
+	img_src = imread("img040.jpg");
+	getInformation(img_src);
+	Mat Roi = img_src(Rect(100, 100, 600, 600));
 	cvtColor(img_src, img_gray, CV_BGR2GRAY);
-	Canny(img_gray, img_canny, 1, 100, 3);
-	linefinder.setThreshold(1);
-	linefinder.setLineLengthAndGap(100, 0);
+	threshold(img_gray, img_bin, 200, 255, THRESH_BINARY | THRESH_OTSU);
+	namedWindow("Bin", WINDOW_FREERATIO);
+	imshow("Bin", img_bin);
+	Canny(img_bin, img_canny, 50, 150, 3,false);
+	namedWindow("Canny", WINDOW_FREERATIO);
+	imshow("Canny", img_canny);
+	linefinder.setThreshold(20);
+	linefinder.setLineLengthAndGap(100, 10);
 	linefinder.findLines(img_canny);
 	linefinder.drawDetectedLines(img_src);
 	namedWindow("figure", WINDOW_FREERATIO);
+	imwrite("ROI.jpg", Roi);
 	imshow("figure", img_src);
-
+	imshow("ROI", Roi);
 	waitKey(0);
 	return 0;
 }
---
+++ Project2/image_information.h
@@ -0,0 +1,2 @@
+#include<vector>
+int src_width, src_height;
